54a2c6959bccb515a4ce2a4b24d3f712342d3b5a
unknown <shakhawatsalam726@gmail.com>
2024-08-21 15:16:17 +0530

    [Hero section is done]

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className='flex justify-center items-center relative top-4'>
+    <div className='flex justify-center items-center fixed top-4 w-full z-10'>
       <nav className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur'>
         <Link href='#' className='nav-item'>
           Home
