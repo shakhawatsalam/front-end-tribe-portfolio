634816a968e2252b2030ec0ca54221b9c78d6ba7
unknown <shakhawatsalam726@gmail.com>
2024-08-21 10:40:01 +0530

    [header section is almost done working on hero section]

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className='flex justify-center items-center relative top-4'>
+      <nav className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur'>
+        <Link href='#' className='nav-item'>
+          Home
+        </Link>
+        <Link href='#' className='nav-item'>
+          Projects
+        </Link>
+        <Link href='#' className='nav-item'>
+          About
+        </Link>
+        <Link href='#' className='nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900'>
+          Contact
+        </Link>
+      </nav>
+    </div>
+  );
 };
